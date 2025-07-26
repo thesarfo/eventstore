03d4741516255a11274c8cf12d68cb3d6328ed6c
Ernest Sarfo <ernestsarfo100@gmail.com>
2025-07-26 21:16:00 +0000

    handle optimistic concurrency

--- EventStore.cs
+++ EventStore.cs
@@ -36,7 +36,37 @@ public class EventStore
         DECLARE
             stream_version int;
         BEGIN
+            --get stream version
+            SELECT version INTO stream_version FROM streams as s WHERE s.id = stream_id FOR UPDATE;
+
+            -- if stream doesnt exist - create new one with version 0
+            IF stream_version IS NULL THEN
+                stream_version := 0;
+
+                INSERT INTO streams (id, type, version) VALUES (stream_id, stream_type, stream_version);
+            END IF;
+
+            -- check optimistic concurrency
+            IF expected_stream_version IS NOT NULL AND stream_version != expected_stream_version THEN
+                RETURN FALSE;
+            END IF;
+
+            -- increment stream version
+            stream_version := stream_version + 1;
+
+            -- append event
+            INSERT INTO events
+            (id, data, type, stream_id, version)
+            VALUES
+            (id, data::jsonb, stream_id, type, stream_version)
+
+            -- update stream version
+            UPDATE streams as s
+                SET version = stream_version
+            WHERE s.id = stream_id;
             RETURN TRUE;
+
+
         END;
         $$";
 
