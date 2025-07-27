9325a5b18693fa6fc1d7fe7c14d7b05e1a21cf97
Ernest Sarfo <ernestsarfo100@gmail.com>
2025-07-27 00:24:59 +0000

    projections

---
+++ src/Cashier.cs
@@ -0,0 +1,28 @@
+namespace eventstore_net;
+
+public record CashierEmployed(
+    Guid CashierId,
+    string Name
+);
+
+public record Cashier(
+    Guid Id,
+    string Name
+)
+{
+    public static Cashier Evolve(Cashier cashier, object @event)
+    {
+        return @event switch
+        {
+            CashierEmployed cashierCreated =>
+                Create(cashierCreated),
+            _ => cashier
+        };
+    }
+
+    public static Cashier Create(CashierEmployed @event) =>
+        new Cashier(
+            @event.CashierId,
+            @event.Name
+        );
+}
