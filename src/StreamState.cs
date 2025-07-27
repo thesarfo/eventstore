5e79c477cbf5e6108af762f5ba16ae14147b8e51
Ernest Sarfo <ernestsarfo100@gmail.com>
2025-07-27 00:23:10 +0000

    implement getting back the state of a stream

---
+++ src/StreamState.cs
@@ -0,0 +1,10 @@
+namespace eventstore_net;
+
+public class StreamState(Guid id, Type type, long version)
+{
+    public Guid Id { get; } = id;
+
+    public Type Type { get; } = type;
+
+    public long Version { get; } = version;
+}
